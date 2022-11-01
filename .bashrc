d4a1675a1aff0d56fb20a161aea0dbb966e6ae15
Louis LAVERNE <louis.laverne@epita.fr>
2022-11-01 17:50:32 +0100

    Script moving bashrc

--- .bashrc
+++ .bashrc
@@ -1,2 +1,3 @@
-feh --bg-scale ~/Epita-XFCE4-Config/bg.jpg
+~/Epita-XFCE4-Config/fetch-master-6000/./fm6000 -l 80 -s "A computer is like air conditioning - it becomes useless when you open Windows.     -Linus Torvalds"
+xrandr -s 1920x1080
 feh --bg-scale ~/Epita-XFCE4-Config/bg.jpg
--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 MESSAGE=\""A computer is like air conditioning - it becomes useless when you open Windows.     -Linus Torvalds\""
+
 git clone https://github.com/anhsirk0/fetch-master-6000.git
 cd fetch-master-6000
 ./install.sh
 cd ~/Epita-XFCE4-Config
-echo "~/Epita-XFCE4-Config/fetch-master-6000/./fm6000 -l 80 -s $MESSAGE" >> ~/.bashrc
-echo "xrandr -s 1920x1080" >> ~/.bashrc
-echo "feh --bg-scale ~/Epita-XFCE4-Config/bg.jpg" >> .bashrc
+
+cp .bashrc ~/.bashrc
+
 cp terminalrc ~/.config/xfce4/terminal/terminalrc
 xrandr -s 1920x1080
 feh --bg-scale bg.jpg
