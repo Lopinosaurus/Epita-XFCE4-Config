a8cb71ae852caed98ebc9d3dcb34a495301cfabf
Louis LAVERNE <louis.laverne@epita.fr>
2022-11-01 17:44:58 +0100

    Removed git repo for fm6000

--- .bashrc
+++ .bashrc
@@ -1,1 +1,2 @@
 feh --bg-scale ~/Epita-XFCE4-Config/bg.jpg
+feh --bg-scale ~/Epita-XFCE4-Config/bg.jpg
--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 MESSAGE=\""A computer is like air conditioning - it becomes useless when you open Windows.     -Linus Torvalds\""
+git clone https://github.com/anhsirk0/fetch-master-6000.git
+cd fetch-master-6000
+./install.sh
+cd ~/Epita-XFCE4-Config
 echo "~/Epita-XFCE4-Config/fetch-master-6000/./fm6000 -l 80 -s $MESSAGE" >> ~/.bashrc
 echo "xrandr -s 1920x1080" >> ~/.bashrc
 echo "feh --bg-scale ~/Epita-XFCE4-Config/bg.jpg" >> .bashrc
