563ed52e4e86dfbebebf6214f49c223d3921d197
Louis LAVERNE <louis.laverne@epita.fr>
2022-11-01 15:47:55 +0100

    Fixed res

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ echo "~/Epita-XFCE4-Config/fetch-master-6000/./fm6000 -l 80 -s $MESSAGE" >> ~/.b
 cp terminalrc ~/.config/xfce4/terminal/terminalrc
 feh --bg-scale bg.jpg
 cp xfce4-desktop.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
+xrandr -s 1920x1080
