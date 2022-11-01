d1fcd2f0985e221ae536829534d3c78448a20dc1
Louis LAVERNE <louis.laverne@epita.fr>
2022-11-01 15:30:38 +0100

    First stable version

---
+++ install.sh
@@ -0,0 +1,5 @@
+MESSAGE=\""A computer is like air conditioning - it becomes useless when you open Windows.     -Linus Torvalds\""
+echo "~/Epita-XFCE4-Config/fetch-master-6000/./fm6000 -l 80 -s $MESSAGE" >> ~/.bashrc
+cp terminalrc ~/.config/xfce4/terminal/terminalrc
+feh --bg-scale bg.jpg
+
