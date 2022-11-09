0e591f4c0782dbf90363e77e09c53f5080d4b33b
Louis Laverne <louis.laverne@epita.fr>
2022-11-09 11:02:10 +0100

    Removed res changes

--- install_sup.sh
+++ install_sup.sh
@@ -6,7 +6,6 @@ mv fm6000 fetch-master-6000/fm6000
 cp .bashrc ~/.bashrc
 
 cp terminalrc ~/.config/xfce4/terminal/terminalrc
-xrandr -s 1920x1080
 feh --bg-scale bg.jpg
 cp xfce4-desktop.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
 
