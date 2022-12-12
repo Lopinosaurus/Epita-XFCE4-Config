46afbcc46166839f2ba5ae94ef1d2ae83a857087
Louis Laverne <louis.laverne@epita.fr>
2022-12-12 15:13:47 +0100

    Touched terminalrc

--- install_sup.sh
+++ install_sup.sh
@@ -1,7 +1,8 @@
 cp .bashrc ~/.bashrc
 
 mkdir ~/.config/xfce/terminal
-touch terminalrc ~/.config/xfce4/terminal/terminalrc
+touch ~/.config/xfce4/terminal/terminalrc
+cp terminalrc ~/.config/xfce4/terminal/terminalrc
 feh --bg-scale bg.jpg
 cp xfce4-desktop.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
 
